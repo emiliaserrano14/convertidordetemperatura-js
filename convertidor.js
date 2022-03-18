609d3d4138145e6dcb38570fd517112b30d1b3c5
chenaoh <emiliaserrano2814@gmail.com>
2022-03-18 17:50:06 -0400

    podras convertir la temperatura de celsius a  fahrenheit

---
+++ convertidor.js
@@ -0,0 +1,9 @@
+// preguntar la temperatura en celsius 
+let temperatura =  parseInt(prompt("cual es la temperatura en celsius?"));
+// convertir celsius a fahrenheit
+let convertToFahrenheit = (temperatura*1.8) + 32;
+// mostrar resultado en la consola 
+console.log(`la temperatura en fahrenheit es ${convertToFahrenheit}`)
+// mostrar en la web
+document.write(`la temperatura en fahrenheit es ${convertToFahrenheit}`)
+
